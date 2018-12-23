89a47a668ed450ce9a4d8c5c62f17574df04eaff
mercury9181 <mercury9181@gmail.com>
2018-12-23 12:22:52 +0600

    mm

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create blog model
+class Blog(models.Model):
+    #ceate structure
+  title = models.CharField(max_length=255)
+  pub_date= models.DateTimeField()
+  body= models.TextField()
+  image = models.ImageField(upload_to='images/')
+
+  #add the blog app in the setings.py
+
+  #create a migration
+
+  #migrate
+
+  #add to the admin.py in blog
--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+#create structure
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    #add the jobs app in the setings.py
+
+    #create a migration
+
+    #migrate
+
+    #add to the admin.py in jobs
+    
